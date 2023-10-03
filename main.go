3010a9630df36d6262c59f5ee422b0a752151666
AqingCyan <aqingcyan@gmail.com>
2023-10-03 22:54:40 +0800

    feat: allow cors

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"mini-ebook/internal/web"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "aqingcyan.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	hdl := web.NewUserHandler()
 	hdl.RegisterRoutes(server)
